8bd6433e8e8298c65e621e11e462b4f33b0b45fc
Arun Govind M <arungovindm@gmail.com>
2021-05-29 19:53:01 +0530

    Add files

---
+++ app.py
@@ -0,0 +1,16 @@
+import app1
+import app2
+import streamlit as st
+
+PAGES = {
+    "Dashboard": app2,
+    "Vaccination Slots": app1
+}
+st.set_page_config(layout='wide',
+                   initial_sidebar_state='expanded',
+                   page_icon="https://www.cowin.gov.in/favicon.ico",
+                   page_title="Covid Dashboard")
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
---
+++ app2.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def app():
+    st.title('Dashboard')
+    st.write('Welcome to the Covid Dashboard')
